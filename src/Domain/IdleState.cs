07c707ea2530ce75d02b719dc0b0e0ae1713e03b
Dan M <dsmiller95@gmail.com>
2025-06-07 18:37:48 -0500

    almost did jumping state

--- src/Domain/IdleState.cs
+++ src/Domain/IdleState.cs
@@ -3,6 +3,8 @@
 using System;
 using Godot;
 
+
+
 public record IdleState(float DeadZone = 0.1f): IState {
   public IState Tick(CharacterContext context) {
     if(Math.Abs(context.Input.MoveAxis) > DeadZone) {
---
+++ src/Domain/JumpingState.cs
@@ -0,0 +1,29 @@
+﻿namespace Plantformer.Domain;
+
+using System;
+using Chickensoft.Log;
+using Godot;
+
+public record JumpingState : IState {
+
+  private float _jumpStartTime;
+
+  public JumpingState(CharacterContext context, float Speed = 50f) {
+    this.Speed = Speed;
+    _jumpStartTime = context.Clock.Now;
+  }
+
+  public IState Tick(CharacterContext context) {
+
+    context.Physics.SetHorizontal(context.Input.MoveAxis * Speed);
+    context.Physics.SetHorizontal(context.Input.MoveAxis * Speed);
+
+    return this;
+  }
+
+  public float Speed { get; init; }
+
+  public void Deconstruct(out float Speed) {
+    Speed = this.Speed;
+  }
+}
